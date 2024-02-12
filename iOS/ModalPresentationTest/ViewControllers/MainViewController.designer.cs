7cad9031622d68bd65740a72862b5c7695c09602
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-02-12 10:21:47 +0000

    Added .NET iOS Reproduction
    This is a .NET iOS native app, I haven't tried the delegate yet but its setup exactly like the swift project so we'll see what happens.

---
+++ iOS/ModalPresentationTest/ViewControllers/MainViewController.designer.cs
@@ -0,0 +1,26 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ModalPresentationTest.ViewControllers
+{
+	[Register ("MainViewController")]
+	partial class MainViewController
+	{
+		[Action ("ShowModal:")]
+		partial void ShowModal (UIKit.UIButton sender);
+
+		[Action ("ShowNavModal:")]
+		partial void ShowNavModal (UIKit.UIButton sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
